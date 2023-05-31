57e14e277622942f09e52a4262332f9eb37c85bb
letafetqurbanli <126787342+letafetqurbanli@users.noreply.github.com>
2023-05-31 13:12:34 +0400

    Add files via upload

---
+++ imtahan 11/asset/js/scipt.js
@@ -0,0 +1,65 @@
+// basket.require({ url: 'jquery.js' });
+// basket.require(
+//     { url: 'jquery.js' },
+//     { url: 'underscore.js' },
+//     { url: 'backbone.js' }
+// );
+// basket.require(
+//     { url: 'require.js' },
+//     { url: 'require.config.js', skipCache: true },
+//     { url: 'libs.js' }
+// );
+// basket
+//     .require({ url: 'jquery.js' })
+//     .then(function () {
+//         basket.require({ url: 'jquery-ui.js' });
+//     });
+
+
+//     basket
+//     .require({ url: 'missing.js' })
+//     .then(function () {
+//         // Success
+//     }, function (error) {
+//         // There was an error fetching the script
+//         console.log(error);
+//     });
+// basket.require({ url: 'jquery.js' });
+
+// basket.require(
+// 	{ url: 'jquery.js' },
+// 	{ url: 'underscore.js' },
+// 	{ url: 'backbone.js' }
+// );
+// basket.require(
+// 	{ url: 'require.js' },
+// 	{ url: 'require.config.js', skipCache: true },
+// 	{ url: 'libs.js' }
+// );
+// basket
+// 	.require({ url: 'jquery.js' })
+// 	.then(function () {
+// 		basket.require({ url: 'jquery-ui.js' });
+// 	});
+
+
+//Deger Ekleme
+// localStorage.setItem("motion1","Push up");
+// localStorage.setItem("motion2","Barfix");
+// localStorage.setItem("motion3","Burpee");
+// localStorage.setItem("motion4","Squat");
+
+//Degeri Almak
+// let value = localStorage.getItem("motion1");
+// console.log(value)
+
+
+// var listele=document.getElementById("listele")
+// let data = fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(response => response.json())
+//     .then(veri => {
+//         veri.forEach(element => {
+            // console.log(element.id);
+            // listele.innerHTML=
+    //     });
+    // });
